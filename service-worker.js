cf16bfd6d406baa4c1d54089e96b752be940607e
SamassaTechnologie <samassatechnologie10@gmail.com>
2026-04-13 22:16:52 +0000

    Refactor service worker caching logic

--- service-worker.js
+++ service-worker.js
@@ -1,43 +1,20 @@
-const CACHE_NAME = "fousseny-cache-v1";
+const CACHE = "fousseny-v1";
 
-const urlsToCache = [
-  "/",
-  "/index.html",
-  "/devis.html",
-  "/facture.html",
-  "/style.css",
-  "/script.js"
-];
-
-// Installation
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-  );
+self.addEventListener("install", e => {
+ e.waitUntil(
+  caches.open(CACHE).then(cache =>
+   cache.addAll([
+    "index.html",
+    "devis.html",
+    "facture.html",
+    "script.js"
+   ])
+  )
+ );
 });
 
-// Activation
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(names => {
-      return Promise.all(
-        names.map(name => {
-          if (name !== CACHE_NAME) {
-            return caches.delete(name);
-          }
-        })
-      );
-    })
-  );
-});
-
-// Fetch (hors ligne)
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        return response || fetch(event.request);
-      })
-  );
+self.addEventListener("fetch", e => {
+ e.respondWith(
+  caches.match(e.request).then(res => res || fetch(e.request))
+ );
 });
