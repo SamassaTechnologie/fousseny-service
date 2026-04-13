f074dc774b8a10e9b9592b45037cb5ef4ac0f29f
SamassaTechnologie <samassatechnologie10@gmail.com>
2026-04-13 01:35:52 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+function addRow() {
+    let table = document.getElementById("table");
+
+    let row = table.insertRow();
+
+    row.innerHTML = `
+    <td><input type="number" value="1" onchange="calc()"></td>
+    <td><input type="text"></td>
+    <td><input type="number" value="0" onchange="calc()"></td>
+    <td class="lineTotal">0</td>
+    `;
+
+    calc();
+}
+
+function calc() {
+    let table = document.getElementById("table");
+    let total = 0;
+
+    for (let i = 1; i < table.rows.length; i++) {
+        let qty = table.rows[i].cells[0].children[0].value;
+        let price = table.rows[i].cells[2].children[0].value;
+
+        let line = qty * price;
+        table.rows[i].cells[3].innerText = line;
+
+        total += line;
+    }
+
+    document.getElementById("total").innerText = total;
+}
---
+++ service-worker.js
@@ -0,0 +1,43 @@
+const CACHE_NAME = "fousseny-cache-v1";
+
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/devis.html",
+  "/facture.html",
+  "/style.css",
+  "/script.js"
+];
+
+// Installation
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// Activation
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(names => {
+      return Promise.all(
+        names.map(name => {
+          if (name !== CACHE_NAME) {
+            return caches.delete(name);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Fetch (hors ligne)
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
